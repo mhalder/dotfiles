eb1a1a53ef23d7df99bd88dfdedd3644d9cc9993
Martin Halder <halderm@arkadia-labs.io>
2025-12-29 01:45:26 +0100

    refactor: minimalist dotfiles overhaul
    - Remove atuin, bash, starship, zsh, alacritty, wezterm
    - Rename terminal/ to ghostty/
    - Optimize configs: git (27 lines), tmux (83 lines), ghostty (7 lines)
    - Add docker/k8s/lazy aliases to fish
    - Update i3: combine xset, switch to flameshot, add parcellite
    - Split setup scripts: setup.sh (CLI/LXC) + setup-desktop.sh (VM)
    - Remove githelpers, tmux-cht, sessionx

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,14 +14,38 @@ if status is-interactive
     abbr -a ss 'cd ../..'
     bind \ee edit_command_buffer  # Alt+e to edit in nvim
 
-    # eza (ls replacement)
+    # eza
     abbr -a l eza
     abbr -a ls eza
-    abbr -a ll eza -l
-    abbr -a la eza -la
-    abbr -a lt eza --tree
-    abbr -a lta eza --tree -a
+    abbr -a ll 'eza -l'
+    abbr -a la 'eza -la'
+    abbr -a lt 'eza --tree'
+
+    # source .env
+    abbr -a se 'export (cat .env | xargs -L 1)'
+
+    # docker cleanup
+    abbr -a drc 'docker rm -f (docker ps -a -q)'
+    abbr -a dri 'docker rmi -f (docker images -q)'
+
+    # lazygit/lazydocker
+    abbr -a lz lazygit
+    abbr -a lzd lazydocker
+
+    # kubernetes
+    abbr -a kx kubectx
+    abbr -a kk k9s
+    abbr -a kn kubens
 
     # Zoxide
     zoxide init fish | source
+
+    # nvm.fish - set default and auto switch on directory change
+    set -g nvm_default_version lts
+    set --query nvm_current_version || nvm use --silent $nvm_default_version
+    function _nvm_auto_use --on-variable PWD
+        if test -f .nvmrc
+            nvm use --silent
+        end
+    end
 end
--- starship/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install starship if not present
-if ! command -v starship &> /dev/null; then
-    echo "Installing starship..."
-    curl -sS https://starship.rs/install.sh | sh
-else
-    echo "Starship is already installed"
-fi
