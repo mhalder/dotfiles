3182c3b09384021198bd290b56f69e5f608bc837
Martin Halder <halderm@arkadia-labs.io>
2026-01-18 22:15:25 +0100

    fix(fish): restore ctrl-p/ctrl-n history navigation in vi mode
    fish_vi_key_bindings doesn't include emacs-style history navigation
    by default. Add explicit bindings for insert mode.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,8 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
     fzf_configure_bindings
+
+    # Restore emacs-style history navigation in insert mode
+    bind -M insert \cp up-or-search
+    bind -M insert \cn down-or-search
 end
