1e39b40af5052ea788560929f7df02c29e9b342d
Martin Halder <halderm@arkadia-labs.io>
2026-03-28 02:37:42 +0100

    fix(fish): bind \cr to fzf history search for Ghostty compatibility
    fzf.fish binds ctrl-r using the new-style key name, but Ghostty sends
    the old-style \cr escape sequence. Add explicit \cr bindings in both
    default and insert mode.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,10 @@ function fish_user_key_bindings
     fish_vi_key_bindings
     fzf_configure_bindings
 
+    # fzf.fish uses ctrl-r (new-style) which Ghostty sends as \cr (old-style)
+    bind \cr _fzf_search_history
+    bind -M insert \cr _fzf_search_history
+
     # Restore emacs-style history navigation in insert mode
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
